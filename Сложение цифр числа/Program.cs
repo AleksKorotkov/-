24c1563b5752c8ee8a00efb08717f0a4ff47e390
Kirill <kirilmarkow16@gmail.com>
2023-10-17 15:53:49 +0900

    Задача сумма цифр числа

---
+++ Сложение цифр числа/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите целое число");
+int a = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (a != 0)
+{
+    int x = a % 10;
+    a = a / 10;
+    sum = sum + x;
+}
+Console.WriteLine("Сумма цифр числа = " + sum);
